954e11fccaa99fa07c46b50e25d005cbab95827f
diyasrivas <diya.srivastava.99@gmail.com>
2026-09-15 13:52:19 +0200

    update module 7 to use synthetic data

--- src/python_for_ia/__init__.py
+++ src/python_for_ia/__init__.py
@@ -4,10 +4,12 @@ from .synthetic import (
     image_with_background,
     images_with_problematic_hist,
     images_with_various_degradations,
+    make_two_channel_cells,
 )
 
 __all__ = [
     "image_with_background",
     "images_with_problematic_hist",
     "images_with_various_degradations",
+    "make_two_channel_cells",
 ]
