499668b719bde7939af445f963430879ff614cac
melisande-c <melisande.croft@fht.org>
2026-09-25 14:02:25 +0200

    06: add question and objectives; convert cells to exercises

--- solutions/06_practical_project.py
+++ solutions/06_practical_project.py
@@ -6,17 +6,16 @@
 # In this project, we are confronted to a real-world image analysis problem. We have data
 # acquired by a collaborator who wants to know the distribution of intensity in the nucleus
 # of their nucleosome target.
-
-# %%
-from python_for_ia import project_data
-from pathlib import Path
-
-# path where to save the data
-parent_folder = Path("data")
-
-# we download the data
-image_folder = project_data(parent_folder)
-print(f"Image path: {image_folder.absolute()}")
+#
+# ### Question
+#
+# - What does it take to extract meaningful insights from images?
+#
+# ### Objective
+#
+# - Get comfortable exploring data.
+# - Apply image analysis operations that we have learned about to real data.
+# - Extract the statistics we are interested in.
 
 # %% [markdown]
 # <div style="
@@ -28,26 +27,50 @@ print(f"Image path: {image_folder.absolute()}")
 #   color: #21457f;
 # ">
 #   <strong style="color: #21457f;">Exercise</strong><br>
+#
 #   Every image analysis project starts  by having a look at the images:
 #
-#   - Load the images in a list
-#   - Print their shape
-#   - For every image, plot both channels next to each other
-#
-#   <b>Hint</b>: if `type(my_path)` is a `Path`, then you can call `.glob("*.tif")` on it
-#   to obtain a list of files. e.g.: `list(my_path.glob("*.tif"))`.
+#   - Load the images in a list and print their shape,
+#   - For each image display the data.
 # </div>
 
 # %%
-from tifffile import imread
+from python_for_ia import project_data
+
+from pathlib import Path
+from skimage.io import imread
 import matplotlib.pyplot as plt
 
+# downloading the data we will work on
+data_path = project_data(Path("../data"))
+
+# Load the files in a list, print their shapes
 # --- Exercise
-files = list(image_folder.glob("*.tif"))
+files = list(Path(data_path).glob("*.tif"))
 images = [imread(f) for f in files]
 for img in images:
     print(f"Image size: {img.shape}")
+# ---
 
+# %% [markdown]
+# <div style="
+#   background: #e8f7ec;
+#   border-left: 6px solid #2f9e44;
+#   padding: 12px 16px;
+#   border-radius: 8px;
+#   margin: 12px 0;
+#   color: #1f5f2c;
+# ">
+#   <strong style="color: #1f5f2c;">Question</strong><br>
+#
+#   What do you the axes are?
+#
+#   Note that these images do not have their metadata saved correctly. This will happen 🤷‍♀️.
+# </div>
+
+# %%
+# Display the data in each image
+# --- Exercise
 fig, axes = plt.subplots(
     len(images),
     3,
@@ -80,6 +103,7 @@ for i in range(len(images)):
 #   color: #1f5f2c;
 # ">
 #   <strong style="color: #1f5f2c;">Question</strong><br>
+#
 #   How would you proceed with the analysis? Which steps would you try?
 # </div>
 #
@@ -92,5 +116,9 @@ for i in range(len(images)):
 #   color: #21457f;
 # ">
 #   <strong style="color: #21457f;">Exercise</strong><br>
+#
 #   Implement a full image analysis pipeline!
 # </div>
+
+# %% [markdown]
+#
--- src/python_for_ia/synthetic.py
+++ src/python_for_ia/synthetic.py
@@ -7,7 +7,7 @@ import pooch
 from pooch.processors import ZipFile
 
 
-def project_data(path: Path) -> list[Path]:
+def project_data(path: Path) -> Path:
     data_path = pooch.retrieve(
         url="https://download.fht.org/jug/teaching_pia/practical_project_data.zip",
         known_hash="021f949c31fa443bfa2fab629a93058e8912d205760fd2e9656f6219308472c9",
